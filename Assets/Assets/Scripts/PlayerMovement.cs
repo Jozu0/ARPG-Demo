3d0e7e441781b03dc4c78166ad53ccdaddf613ff
Jozu0 <russo.adam06@gmail.com>
2025-02-28 11:33:42 +0100

    movement 1.0

--- Assets/Assets/Scripts/PlayerMovement.cs
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,61 @@
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
     public Rigidbody2D rb;
+    public Animator anim;
 
+    private bool facingRight = true;
     public float speed = 1f;
-    private float horizontal;
-    private float vertical;
     private Vector3 movement;
+    private Vector2 lastMovementDirection;
 
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
 
     void Update() {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        ProcessInputs();
+        Animate();
+        if(movement.x>0 && !facingRight ||movement.x<0 && facingRight){
+            Flip();
+        }
     }
     void FixedUpdate()
     {
-        movement = new Vector3(horizontal, vertical, 0);
-        rb.linearVelocity = new Vector3(movement.x * speed, movement.y * speed);
+        rb.linearVelocity = movement*speed;
+    }
+
+    void Flip(){
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        facingRight = !facingRight;
+    }
+
+    void ProcessInputs(){
+        int moveX = (int)Input.GetAxisRaw("Horizontal");
+        int moveY = (int)Input.GetAxisRaw("Vertical");
+        if((moveX == 0 && moveY == 0) &&  (movement.x !=0 || movement.y != 0)){
+            lastMovementDirection = movement;
+        }
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+    }
+
+    void Animate(){
+        anim.SetFloat("MoveX",movement.x);
+        anim.SetFloat("MoveY",movement.y);
+        anim.SetFloat("LastX",lastMovementDirection.x);
+        anim.SetFloat("LastY",lastMovementDirection.y);
+        anim.SetFloat("MoveMagnitude",movement.magnitude);
     }
 }
